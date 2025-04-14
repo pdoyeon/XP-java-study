1c823e538b985fa35ae1b5cd6040d6c7cbb35500
pdoyeon <pdoyeon06@gmail.com>
2025-04-14 15:47:25 +0900

    print문

---
+++ Chap2/FirstJava.java
@@ -0,0 +1,18 @@
+package Chap2;
+
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        System.out.println("Hello Seoul!");
+
+        System.out.println("Hello Busan!");
+
+        System.out.println("Hello jeju!");
+
+        System.out.println("Hello Beawha!");
+
+    }
+}
+
+// debug : 코딩 과정을 볼 수 있음
